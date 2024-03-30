1506cf75df01ada75090ef8c6042a432c27592f4
Ilya <107246761+illya-s@users.noreply.github.com>
2024-03-30 16:44:22 +0200

    update

--- test.py
+++ test.py
@@ -1,48 +1,33 @@
-# from taglib import File
-
-# def set_wav_metadata(file_path, title, artist, album):
-#     with File(file_path, save_on_exit=True) as song:
-#         song.tags["ALBUM"] = [album]
-#         song.tags["GENRE"] = ["Vocal", "Classical"]
-#         song.tags["PERFORMER"] = ["Ton Koopman"]
-# # {'ARTIST': ['piman', 'jzig'], 'ALBUM': ['Quod Libet Test Data'], 'TITLE': ['Silence'], 'GENRE': ['Silence'], 'TRACKNUMBER': ['02/10'], 'DATE': ['2004']}
-
-
-# # Example usage
-# file = r"C:\Users\Ilya\Desktop\test\За Тебя.wav"
-# set_wav_metadata(file, 'My Song', 'Artist Name', 'Album Name')
-
 from pytube import YouTube
+from taglib import File
+from pathlib import Path
 
-def get_video_metadata(url):
-    try:
-        yt = YouTube(url)
-        metadata = {
-            'title': yt.title,
-            'author': yt.author,
-            'description': yt.description,
-            'views': yt.views,
-            'length': yt.length,
-            'rating': yt.rating,
-            'thumbnail_url': yt.thumbnail_url,
-            'keywords': yt.keywords,
-            'streams': yt.streams
-        }
-        return metadata
-    except Exception as e:
-        print("Error:", e)
-        return None
 
-# Example usage
-video_url = "https://music.youtube.com/watch?v=4ebBW-gzSNw&si=_Kkj3Te0l18uFkIy"  # Rick Astley - Never Gonna Give You Up
-metadata = get_video_metadata(video_url)
-if metadata:
-    print("Title:", metadata['title'])
-    print("Author:", metadata['author'])
-    print("Description:", metadata['description'])
-    print("Views:", metadata['views'])
-    print("Length:", metadata['length'])
-    print("Rating:", metadata['rating'])
-    print("Thumbnail URL:", metadata['thumbnail_url'])
-    print("Keywords:", metadata['keywords'])
-    print("Streams:", metadata['streams'])
+video_url = "https://music.youtube.com/watch?v=4ebBW-gzSNw&si=_Kkj3Te0l18uFkIy"
+yt = YouTube(video_url)
+
+
+def set_wav_metadata(file_path):
+    with File(file_path, save_on_exit=True) as audio:
+        audio.tags["TITLE"] = [yt.title.encode(encoding='utf_8', errors="ignore")]
+        audio.tags["ALBUM"] = [yt.author.encode(encoding='utf_8', errors="ignore")]
+        audio.tags["PERFORMER:HARPSICHORD"] = [yt.author.encode(encoding='utf-8', errors="ignore")]
+
+
+def get_audio_metadata(file_path):
+    audio_file = File(file_path)
+    metadata = {
+        "title": audio_file.tags.get("TITLE", [""])[0],
+        "artist": audio_file.tags.get("ARTIST", [""])[0],
+        "album": audio_file.tags.get("ALBUM", [""])[0],
+    }
+    return metadata
+
+
+file = Path("C:/Users/Ilya/Desktop/test/За Тебя.wav")
+set_wav_metadata(file)
+metadata = get_audio_metadata(file)
+
+print("Title:", metadata["title"])
+print("Artist:", metadata["artist"])
+print("Album:", metadata["album"])
